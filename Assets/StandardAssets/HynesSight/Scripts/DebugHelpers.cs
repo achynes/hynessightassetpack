1340561fc659efaa6883d70dc202cb57037f3725
Andrew Hynes <achynes@gmail.com>
2024-05-14 20:24:39 +0100

    Add a couple of little DebugHelper functions.

--- Assets/StandardAssets/HynesSight/Scripts/DebugHelpers.cs
+++ Assets/StandardAssets/HynesSight/Scripts/DebugHelpers.cs
@@ -8,5 +8,21 @@ namespace HynesSight
 		{
 			Debug.LogError("This 'default' case should never be hit.");
 		}
+
+		public static void PrintSwitchUnusedCaseError<T>(T unusedCase)
+		{
+			Debug.LogErrorFormat("%s case should never be hit.", unusedCase);
+		}
+
+		public static void Assert(bool condition, string errorMessage, params object[] errorParams)
+		{
+			if (!condition)
+			{
+				if (errorParams.Length > 0)
+					Debug.LogErrorFormat(errorMessage, errorParams);
+				else
+					Debug.LogError(errorMessage);
+			}
+		}
 	}
 }
