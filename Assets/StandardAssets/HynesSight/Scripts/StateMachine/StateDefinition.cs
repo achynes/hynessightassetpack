5811251e39013ea526bb55a8278ecf13ef6a95c4
Andrew Hynes <achynes@gmail.com>
2024-05-14 20:24:30 +0100

    Move StateMachine towards something simpler and more data driven. Hasn't been used yet, but for sure it would be good to add some kind of category system to attribute states to the state machines they belong to, so multiple machines can exist in one project without overlap.

---
+++ Assets/StandardAssets/HynesSight/Scripts/StateMachine/StateDefinition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace HynesSight.StateMachine
+{
+    public class StateDefinition : ScriptableObject
+	{
+		public virtual void OnEnter(StateDefinition previousState, params object[] stateChangeData) { }
+		public virtual void OnExit() { }
+		public virtual void OnUpdate(float deltaTime) { }
+		public virtual void OnFixedUpdate(float fixedDeltaTime) { }
+		public virtual void OnNotify(params object[] notificationData) { }
+
+		public virtual bool CanEnter(StateDefinition previousState)
+		{
+			return true;
+		}
+	}
+}
--- Assets/StandardAssets/HynesSight/Scripts/StateMachine/StateMachine.cs
+++ Assets/StandardAssets/HynesSight/Scripts/StateMachine/StateMachine.cs
@@ -3,91 +3,47 @@ using System.Collections.Generic;
 
 namespace HynesSight.StateMachine
 {
-    // Populate this enum with the necessary StateTypes for the game being made.
-    public enum StateType
-    {
-        None = 0,
-        //Example = 1
-    }
-
-    /// <summary>
-    /// A generic StateMachine. Override the State_Base class and add them to the StateClasses Dictionary to create new states.
-    /// To use this, it is attached as a Component to a GameObject, where you can specify which states to allow on that instance of the StateMachine.
-    /// </summary>
     public sealed class StateMachine : MonoBehaviour
     {
-        // Populate this Dictionary with keys from the StateType enum and the corresponding classes inherited from State_Base.
-        private static Dictionary<StateType, System.Type> StateClasses = new Dictionary<StateType, System.Type>()
+		// The state machine will enter the 0 entry state on start.
+		[SerializeField]
+        List<StateDefinition> _states = null;
+
+		List<StateDefinition> _stateInstances = null;
+
+		public StateDefinition CurrentState { get; private set; }
+
+		void Awake()
         {
-            //{StateType.Example, typeof(State_Example) },
-        };
-
-        private Dictionary<StateType, State_Base> StateInstances;
-
-        [SerializeField]
-        private StateType[] AvailableStates;
-
-        [SerializeField]
-        private StateType StartingState;
-
-		public bool _useUnscaledDeltaTime;
-
-        public State_Base CurrentState { get; private set; }
-
-        public void Init()
-        {
-            if (StateClasses.Count == 0)
-            {
+            if (_states.Count == 0)
                 return;
-            }
 
-            StateInstances = new Dictionary<StateType, State_Base>(StateClasses.Count);
+            _stateInstances = new List<StateDefinition>(_states.Count);
 
-            for (int n = AvailableStates.Length - 1; n > -1; n--)
+            foreach (StateDefinition state in _states)
             {
-                if (!StateClasses.ContainsKey(AvailableStates[n]))
-                {
-                    Debug.LogError("Trying to instantiate a StateType that is not in the StateClasses Dictionary.");
-                    continue;
-                }
+				StateDefinition newStateDefinition = (StateDefinition)System.Activator.CreateInstance(state.GetType());
+				_stateInstances.Add(newStateDefinition);
 
-                State_Base StateInstance = (State_Base)System.Activator.CreateInstance(StateClasses[AvailableStates[n]]);
-                StateInstances.Add(AvailableStates[n], StateInstance);
-                
-                if (null == CurrentState)
-                {
-                    CurrentState = StateInstance;
-                }
-            }
-
-            if (StateInstances.ContainsKey(StartingState))
-            {
-                CurrentState = StateInstances[StartingState];
-            }
-            else
-            {
-                Debug.LogError("StateInstances does not contain the StartingState specified.");
+				if (null == CurrentState)
+					CurrentState = newStateDefinition;
             }
         }
-        
-        public bool ChangeState(StateType stateType, params object[] stateChangeData)
+
+		public bool ChangeState(StateDefinition nextState, params object[] stateChangeData)
         {
-            StateType previousStateType = StateType.None;
-            State_Base nextState = StateInstances[stateType];
-
-            if (!nextState.CanEnter(previousStateType))
-            {
+            if (!nextState.CanEnter(CurrentState))
                 return false;
-            }
 
+			StateDefinition previousState = null;
             if (null != CurrentState)
-            {
-                previousStateType = CurrentState.StateType;
+			{
+				previousState = CurrentState;
                 CurrentState.OnExit();
-            }
+			}
 
             CurrentState = nextState;
-            CurrentState.OnEnter(previousStateType, stateChangeData);
+            CurrentState.OnEnter(previousState, stateChangeData);
 
             return true;
         }
@@ -95,22 +51,18 @@ namespace HynesSight.StateMachine
         private void Update()
         {
             if (null != CurrentState)
-            {
-                CurrentState.Update(_useUnscaledDeltaTime ? Time.unscaledDeltaTime : Time.deltaTime);
-            }
+                CurrentState.OnUpdate(Time.deltaTime);
         }
 
 		private void FixedUpdate()
 		{
 			if (null != CurrentState)
-			{
-				CurrentState.FixedUpdate(Time.fixedDeltaTime);
-			}
+				CurrentState.OnFixedUpdate(Time.fixedDeltaTime);
 		}
 
 		public void NotifyState(params object[] notificationData)
         {
-            CurrentState.Notify(notificationData);
+            CurrentState.OnNotify(notificationData);
         }
     }
 }
